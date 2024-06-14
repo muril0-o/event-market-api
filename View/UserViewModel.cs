0580a3542577ab742ce012b16f3e620d98548b4a
muriloliveira <murilolives20@gmail.com>
2024-06-13 21:24:48 -0300

    Adding nullable dtype.

--- View/UserViewModel.cs
+++ View/UserViewModel.cs
@@ -5,22 +5,22 @@ namespace View
     public class UserViewModel
     {
         [Required]
-        public string txt_first_name { get; set;}
+        public string? txt_first_name { get; set;}
         [Required]
-        public string txt_last_name { get; set; }
+        public string? txt_last_name { get; set; }
         [Required]
-        public string txt_cpf { get;  set; }
+        public string? txt_cpf { get;  set; }
         [Required]
         public DateTime dt_birth_date { get; set;} 
         [Required]
         public int vl_age { get; set; }
         [Required]
-        public string txt_phone_number { get; set; }
+        public string? txt_phone_number { get; set; }
         [Required]
-        public string txt_username { get; set; }
+        public string? txt_username { get; set; }
         [Required]
-        public string txt_email { get; set; }
+        public string? txt_email { get; set; }
         [Required]
-        public string txt_hash_password { get; set; }
+        public string? txt_hash_password { get; set; }
     }
 }
